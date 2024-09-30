33e9a0787efefd809437412243c7726609d00de7
Mohamad Risky <138768532+Risky666@users.noreply.github.com>
2024-09-30 09:14:03 +0700

    Initial commit

--- app/src/main/java/com/example/happybirthdayrisky/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayrisky/MainActivity.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.happybirthdayrisky.ui.theme.HappyBirthdayRiskyTheme // Pastikan impor tema benar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,16 +24,11 @@ class MainActivity : ComponentActivity() {
         // setContent digunakan untuk mendefinisikan tampilan UI berbasis Compose
         setContent {
             HappyBirthdayRiskyTheme {
-                // Menggunakan tema HappyBirthdayRiskyTheme untuk menerapkan skema warna dan gaya
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    // Menampilkan komponen GreetingText yang memuat pesan ulang tahun
-                    GreetingText(
                         message = "Happy Birthday Risky!",
-                        from = "From Nadilla",
-                        modifier = Modifier.padding(8.dp)
                     )
                 }
             }
@@ -47,30 +41,20 @@ class MainActivity : ComponentActivity() {
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
-        modifier = modifier.fillMaxSize()
     ) {
         Text(
-            text = message,//pesan yang akan ditampilkan
-            fontSize = 100.sp,//ukuran teks
-            lineHeight = 116.sp,//jarak antar baris teks
-            textAlign = TextAlign.Center,//mengatur teks agar berada ditengah
             modifier = Modifier.align(Alignment.CenterHorizontally) // Mengatur agar teks berada di tengah secara horizontal
         )
         Text(
-            text = from, //nama pengirim yang akan ditampilkan
             fontSize = 36.sp,
             modifier = Modifier
-                .padding(16.dp)//memberikan padding sekitar teks
-                .align(Alignment.End)//menempatkan teks disudut kanan bawah
         )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayRiskyTheme {
-        GreetingText(message = "Happy Birthday Risky", from = "From Nadilla")
     }
 }
